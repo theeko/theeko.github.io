3dc7579fc09e5f12e8d93f9174e25d1d4a775693
kairath <thekairath@hotmail.com>
2015-11-24 00:26:05 +0000

    header updated-icons, fonts added.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("myApp", []);
+
+app.directive("icons", function(){
+   return {
+       restrict: "E",
+       templateUrl: "icons.html",
+   } 
+});
---
+++ js/portfolioctrl.js
@@ -0,0 +1,13 @@
+app.controller("PortfolioCtrl", [ "$scope", function($scope){
+    $scope.showcases = [
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+        { link: "img/angularcode.jpg" },
+    ]
+}]);
