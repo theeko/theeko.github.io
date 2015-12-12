a397ef7ecc1a14d1b679319d68b81ef276f03597
kairath <thekairath@hotmail.com>
2015-12-12 00:25:40 +0000

    new project added

--- js/portfolioctrl.js
+++ js/portfolioctrl.js
@@ -1,20 +1,27 @@
+"use strict";
+
 app.controller("PortfolioCtrl", [ "$scope", function($scope){
-    $scope.showcases = [
-        { path: "img/promodoro.PNG", name: "Promodoro Clock", 
-        descrtiption: "promodoro clock",
-        link: "http://thekairath.github.io/FrontEndProjectz/PromodoroClock"
-        },
-        { path: "img/tictactoe.PNG", name: "TicTacToe", 
-        descrtiption: "tictactoe game",
-        link: "http://thekairath.github.io/FrontEndProjectz/tictactoe"
-        },
-        { path: "img/simongame.PNG", name: "Simon Game", 
-        descrtiption: "asd asd asd asd",
-        link: "http://thekairath.github.io/FrontEndProjectz/simongame/"
-        },
-        { path: "img/wikipediaapi.PNG", name: "Wikipedia Api", 
-        descrtiption: "asd asd asd asd",
-        link: "http://thekairath.github.io/FrontEndProjectz/wikipediaapi/"
-        }
-    ]
+$scope.showcases = [
+    { path: "img/meanvote.PNG", name: "Meanstack Vote App", 
+    descrtiption: "asd asd asd asd",
+    link: "http://guarded-crag-3272.herokuapp.com/"
+    },
+    { path: "img/promodoro.PNG", name: "Promodoro Clock", 
+    descrtiption: "promodoro clock",
+    link: "http://thekairath.github.io/FrontEndProjectz/PromodoroClock"
+    },
+    { path: "img/tictactoe.PNG", name: "TicTacToe", 
+    descrtiption: "tictactoe game",
+    link: "http://thekairath.github.io/FrontEndProjectz/tictactoe"
+    },
+    { path: "img/simongame.PNG", name: "Simon Game", 
+    descrtiption: "asd asd asd asd",
+    link: "http://thekairath.github.io/FrontEndProjectz/simongame/"
+    },
+    { path: "img/wikipediaapi.PNG", name: "Wikipedia Api", 
+    descrtiption: "asd asd asd asd",
+    link: "http://thekairath.github.io/FrontEndProjectz/wikipediaapi/"
+    }
+    
+];
 }]);
