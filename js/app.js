c62896519b4cc55578d9a4ce7d233031f4de427a
kairath <thekairath@hotmail.com>
2015-12-12 14:56:05 +0000

    minorfix

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var app = angular.module("myApp", []);
 
-// app.directive("icons", function(){
-//    return {
-//        restrict: "E",
-//        templateUrl: "icons.html",
-//    } 
-// });
+app.directive("icons", function(){
+    return {
+        restrict: "E",
+        templateUrl: "icons.html",
+    } 
+});
