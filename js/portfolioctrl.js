9a5c2c5ac009be241594aaf5d31d2a43072dee97
kairath <thekairath@hotmail.com>
2016-06-07 23:57:10 +0000

    minor changes- google analytics added

--- js/portfolioctrl.js
+++ js/portfolioctrl.js
@@ -6,45 +6,45 @@ $scope.showcases = [
     descrtiption: "asd asd asd asd",
     link: "https://dentist-site-eng.herokuapp.com/"
     },
-    { path: "img/gameOfLife.PNG", name: "React/GameofLife", 
-    descrtiption: "asd asd asd asd",
-    link: "http://codepen.io/kairath/full/vLeKbX/"
-    },
-    { path: "img/meanvote.PNG", name: "Mean Vote App", 
-    descrtiption: "asd asd asd asd",
-    link: "http://guarded-crag-3272.herokuapp.com/"
-    },
-    { path: "img/pintclone.PNG", name: "PinterestClone", 
-    descrtiption: "asd asd asd asd",
-    link: "https://vast-thicket-9171.herokuapp.com/"
-    },
-    { path: "img/booktrade.PNG", name: "BooktradeApp", 
-    descrtiption: "asd asd asd asd",
-    link: "https://intense-mesa-3678.herokuapp.com"
-    },
-    { path: "img/d3project3.PNG", name: "D3 HeatMap", 
-    descrtiption: "asd asd asd asd",
-    link: "http://thekairath.github.io/FrontEndProjectz/d3project3/"
-    },
     { path: "img/recipebox.PNG", name: "React/Recipebox", 
     descrtiption: "asd asd asd asd",
     link: "http://codepen.io/kairath/full/KVvebq/"
-    },
-    { path: "img/d3meteor.PNG", name: "D3 Meteor", 
-    descrtiption: "asd asd asd asd",
-    link: "http://codepen.io/kairath/full/bEKwvV/"
     },
     { path: "img/react-markdown.PNG", name: "React/MarkdownnPrev", 
     descrtiption: "asd asd asd asd",
     link: "http://codepen.io/kairath/full/ZQyEWP/"
     },
-    { path: "img/reactladderboard.PNG", name: "React/AjaxLadderboard", 
-    descrtiption: "asd asd asd asd",
-    link: "http://codepen.io/kairath/full/EVOvBM/"
-    },
     { path: "img/nightlife.PNG", name: "NightlifeApp", 
     descrtiption: "asd asd asd asd",
     link: "https://serene-escarpment-3856.herokuapp.com/"
+    },
+    { path: "img/meanvote.PNG", name: "Mean Vote App", 
+    descrtiption: "asd asd asd asd",
+    link: "http://guarded-crag-3272.herokuapp.com/"
+    },
+    { path: "img/booktrade.PNG", name: "BooktradeApp", 
+    descrtiption: "asd asd asd asd",
+    link: "https://intense-mesa-3678.herokuapp.com"
+    },
+    { path: "img/gameOfLife.PNG", name: "React/GameofLife", 
+    descrtiption: "asd asd asd asd",
+    link: "http://codepen.io/kairath/full/vLeKbX/"
+    },
+    { path: "img/pintclone.PNG", name: "PinterestClone", 
+    descrtiption: "asd asd asd asd",
+    link: "https://vast-thicket-9171.herokuapp.com/"
+    },
+    { path: "img/d3project3.PNG", name: "D3 HeatMap", 
+    descrtiption: "asd asd asd asd",
+    link: "http://thekairath.github.io/FrontEndProjectz/d3project3/"
+    },
+    { path: "img/d3meteor.PNG", name: "D3 Meteor", 
+    descrtiption: "asd asd asd asd",
+    link: "http://codepen.io/kairath/full/bEKwvV/"
+    },
+    { path: "img/reactladderboard.PNG", name: "React/AjaxLadderboard", 
+    descrtiption: "asd asd asd asd",
+    link: "http://codepen.io/kairath/full/EVOvBM/"
     },
     { path: "img/promodoro.PNG", name: "PromodoroClock", 
     descrtiption: "promodoro clock",
